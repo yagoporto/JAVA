df14e805642ac8482d3f6231e2df599bfe2436c4
Yago Porto <yagosouz8@gmail.com>
2023-09-13 21:54:21 -0300

    tipos primitvos

---
+++ tiposVariaveis.java
@@ -0,0 +1,19 @@
+package JAVA;
+
+public class tiposVariaveis {
+    public static void main(String[] args) {
+        int idade; //Tipo "int", nome "idade", não foi atribuido valor
+        int anoFabricação = 2021; // tipo "int", nome "anoFabricação", valor atribuido "2021"
+        double salrioBruto = 2500; //tipo "double", nome "salarioBruto", valor atribuido "2.500" 
+        
+        
+        
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo, e termina com "l"
+        float pi = 3.14F; //termina com "f"
+        double salario = 1275.33;  
+    }
+    
+}
