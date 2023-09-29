cc2daaa95c2d7cef75d3608ed3c879cd81ebc43d
Yago Porto <yagosouz8@gmail.com>
2023-09-28 21:17:45 -0300

    PalavrasReservadas

--- escopo/src/EscopoConta.java
+++ escopo/src/EscopoConta.java
@@ -10,7 +10,7 @@ public class EscopoConta {
 		//disponível em toda classe
 		System.out.println(saldo);
 		//somente o método sacar conhece esta variavel
-		System.out.println(novoSaldo);
+		//System.out.println(novoSaldo);
 	
 	}
 	public double calcularDividaExponencial(){
