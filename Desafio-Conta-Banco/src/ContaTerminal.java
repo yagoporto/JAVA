b89a9c9a20ad87851f8bf7d190990fe6a1adc5b3
Yago Porto <yagosouz8@gmail.com>
2023-09-29 20:09:23 -0300

    Desafio de sintaxe

---
+++ Desafio-Conta-Banco/src/ContaTerminal.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class ContaTerminal { 
+
+    public static void main(String[] args) {
+        int numeroConta;
+        String numeroAgencia, nomeDoCliente;
+        double saldo = 1715.38;
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("======================================");
+        System.out.println("|            BANCO JAVA              |");
+        System.out.println("======================================");
+        System.out.println();
+
+        System.out.print("Digite o nome do cliente: ");
+        nomeDoCliente = sc.nextLine();
+        System.out.print("Digite o número da agencia: ");
+        numeroAgencia = sc.nextLine();
+        System.out.print("Digite o número da conta: ");
+        numeroConta = sc.nextInt();
+
+        System.out.printf("Olá %s, obrigado por acessar o nosso banco, sua agencia é %s, conta %d e seu saldo de R$%.2f já esta disponível para saque", nomeDoCliente, numeroAgencia, numeroConta, saldo);
+        
+    
+    }
+    
+}
